3bcc253e27b11db9fa377323d8115caa3a57a961
VNRESOURCE\hoa.tranvan <hoa.tranvan@vnresource.org>
2025-04-11 17:08:32 +0700

    fix: update profile image format from PNG to JPG

--- src/utils/data/personal-data.ts
+++ src/utils/data/personal-data.ts
@@ -1,6 +1,6 @@
 export const personalData = {
   name: 'Hoa',
-  profile: 'assets/profile.png',
+  profile: 'assets/profile.jpg',
   designation: 'Software Developer',
   description:
     "I'm Hoa, a Frontend Developer with 2 years of hands-on experience specializing in building clean, responsive, and high-performance web applications using Angular. I focus on delivering seamless user experiences by writing maintainable, scalable code and optimizing UI performance.\n\nAlongside Angular, I’m familiar with backend integration via RESTful APIs, JWT authentication, and state management with NgRx. My understanding of tools like RxJS, Tailwind CSS, and Git helps me collaborate effectively in agile teams.\n\nI’m continuously learning to stay up-to-date with evolving technologies and best practices, always aiming to build reliable and impactful products.",
